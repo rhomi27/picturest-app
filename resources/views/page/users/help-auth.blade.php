f8718f3c1564a1658a09f61eadf629b18105a1d0
rhomi27 <romisaja556@gmail.com>
2024-02-28 14:41:37 +0700

    perbaikan

--- resources/views/page/users/help-auth.blade.php
+++ resources/views/page/users/help-auth.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.master')
 @section('content')
     <div class="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-md relative mt-5">
-        <button onclick="goBack()" class="absolute right-1 top-1 text-blue-900 text-xs rounded-md mb-4 hover:text-blue-600 ">
+        <a href="/profil" class="absolute right-1 top-1 text-blue-900 text-xs rounded-md mb-4 hover:text-blue-600 ">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 scale-100 hover:scale-110" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg></button>
+            </svg></a>
 
         <!-- Section Tentang Aplikasi -->
         <section id="tentang" class="mb-8">
